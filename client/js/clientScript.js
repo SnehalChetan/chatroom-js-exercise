bd98e79c66d2c5ffc37d9978b136ba2b3d8f92b0
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-20 10:52:37 +0200

    add broadcast to server js and target message div to display chat to index

--- client/js/clientScript.js
+++ client/js/clientScript.js
@@ -1,1 +1,52 @@
+/*
 let socket = io.connect();
+
+const clientMessageForm = document.getElementById("clientMessageForm");
+const  messageInput = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
+let user = prompt("Enter your name",);
+
+clientMessageForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (messageInput.value) {
+        socket.emit('sendMessage',user +' : '+ messageInput.value);
+        messageInput.value = ' ';
+    }
+});
+
+
+socket.on('sendMessage', (msg) => {
+    const messageElement = document.createElement('h4');
+    messageElement.innerText = msg;
+    messages.append(messageElement);/!**!/
+
+
+});
+*/
+
+let socket = io.connect();
+
+
+const theInput = document.getElementById('sendForm');
+const display = document.getElementById('display');
+const input = document.getElementById('message');
+
+let userName = prompt('choose your user name');
+
+theInput.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = input.value;
+    socket.emit('sendMessage', userName +': ' + message);
+    input.value = ' ';
+})
+
+socket.on('sendMessage', data => {
+    displayMessage(data);
+})
+
+function displayMessage(message){
+    const popup = messageElement = document.createElement('h4');
+    popup.innerText = message;
+    display.append(popup);
+
+}
--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
+
 const express = require('express');
 const http = require('http');
-/*use express and http to make it easy to host our client*/
-const app = express();/*To define our application*/
-const clientPath = `${__dirname}/../client`; /*To give the path to our client*/
-app.use(express.static(clientPath));/*To use express to host the client*/
-const server = http.createServer(app);/*To use http to serve the app that express provides*/
-const port = 8080;
-const io = require('socket.io')(server);
 
-server.listen(port, () =>{
+/!*use express and http to make it easy to host our client*!/
+
+const app = express();/!*To define our application*!/
+const clientPath = `${__dirname}/../client`; /!*To give the path to our client*!/
+
+app.use(express.static(clientPath));/!*To use express to host the client*!/
+
+const server = http.createServer(app);/!*To use http to serve the app that express provides*!/
+
+const port = 8080;
+
+
+server.listen(8080, () =>{
     console.log("server running on "+port);
 });
-let counter = 0
-io.on('connection', (socket) => {
-    counter++;
-    console.log(counter+'  someone connected ');
+const io = require('socket.io')(server);
+
+/* work work work work work work work work work work*/
+
+io.on('connection', socket => {
+    socket.on('sendMessage', message => {
+        socket.broadcast.emit('sendMessage', message);
+    })
+
 });
